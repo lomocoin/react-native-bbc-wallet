a19d377316098275cb34532db0f81ec91fee954a
ZhangRGK <stamen0630@gmail.com>
2020-03-28 20:14:00 +0800

    feat: Add type declaration

---
+++ index.d.ts
@@ -0,0 +1,17 @@
+export interface KeyInfo {
+  privateKey: string;
+  publicKey: string;
+  address: string;
+}
+
+declare module RNBbcWallet {
+  function generateMnemonic(): Promise<string>;
+  function importMnemonic(mnemonic: string, salt: string): Promise<KeyInfo>;
+  function importPrivateKey(privateKey: string): Promise<string>;
+  function signTransaction(
+    txString: string,
+    privateKey: string
+  ): Promise<string>;
+}
+
+export default RNBbcWallet;
